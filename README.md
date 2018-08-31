7f5613b797d25c5fe692a1403bd25874d22c30a6
t3chn0tr0n <technotron.avik@gmail.com>
2018-08-31 20:09:51 +0530

    start django app - add a gitignore - add requirements in README - connect postgresql server - add email settings - change timezone to kolkata - add global template and static locations

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # nit_project
 a django project for creating and maintaining the student database of my college
+
+# Requirements:
+1. Django_2.1.1 
+2. Python 3 (3.6 used)
+3. PostgreSQL
+4. psycorg2
+    
+Note: migrations are in gitignore file. So, after each clone, please run `python manage.py makemigrations`
---
+++ server_configs/settings.py
@@ -0,0 +1,111 @@
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+global_statics = os.path.join(BASE_DIR, 'resources/static/') 
+global_templates = os.path.join(BASE_DIR, 'resources/templates/')
+
+
+SECRET_KEY = 'mpn@rt2shr#-hs_7ow+(=%=%#+-c-m6u7q0ibaaiswz&%+9s%3'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'server_configs.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [global_templates],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'server_configs.wsgi.application'
+
+# Database
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'nitdb',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+# Password validation
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+# Internationalization
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'Asia/Kolkata'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+# Static files (CSS, JavaScript, Images)
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'resources/statics')
+STATICFILES_DIRS = [
+    global_statics,
+	]
+
+# Email settings (Emails are used for a/c varification)
+
+EMAIL_USE_TLS  = True
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
